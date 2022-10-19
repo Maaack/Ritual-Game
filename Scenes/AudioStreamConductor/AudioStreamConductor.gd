151da611f56978ea2514ae8b5014843e73b6b687
Maaack <marek.belski@gmail.com>
2022-10-18 18:36:05 -0700

    Rename, refactor, and reduce code.

---
+++ Scenes/AudioStreamConductor/AudioStreamConductor.gd
@@ -0,0 +1,32 @@
+extends AudioStreamPlayer
+
+const SECONDS_PER_MINUTE = 60.0
+
+export(int) var beats_per_minute : int = 100
+export(int) var beats_per_measure : int = 4
+export(float) var track_offset : float = 0.0
+
+var song_position = 0.0
+var song_position_in_beats = 0
+
+
+signal beat(total, in_measure)
+
+func get_seconds_per_beat():
+	return SECONDS_PER_MINUTE / beats_per_minute
+
+func _physics_process(delta):
+	if playing:
+		song_position = get_playback_position() + AudioServer.get_time_since_last_mix()
+		song_position -= AudioServer.get_output_latency()
+		_report_beat()
+
+func _report_beat():
+	var next_song_position_in_beats : int = int(floor(song_position / get_seconds_per_beat()))
+	if song_position_in_beats < next_song_position_in_beats:
+		song_position_in_beats = next_song_position_in_beats
+		emit_signal("beat", song_position_in_beats, song_position_in_beats % beats_per_measure)
+
+func play_from_beat(beat, offset = track_offset):
+	play()
+	seek(beat * get_seconds_per_beat() + offset)
