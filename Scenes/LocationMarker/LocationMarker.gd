4c224244dba15f6f3309418662e54c341f533e00
L0G1C <contact.leojerez@gmail.com>
2022-10-22 23:53:00 -0400

    updating to yield in start_timer()

--- Scenes/LocationMarker/LocationMarker.gd
+++ Scenes/LocationMarker/LocationMarker.gd
@@ -5,8 +5,6 @@ extends Control
 export(Resource) var location_data : Resource setget set_location_data
 export(Array, NodePath) var neighbors : Array = []
 
-var onCooldown = false
-
 func set_location_data(value : Resource) -> void:
 	location_data = value
 	var label = get_node_or_null("Label")
@@ -17,14 +15,9 @@ func set_location_data(value : Resource) -> void:
 func _ready():
 	self.location_data = location_data
 	
-func _process(_delta):
-	if onCooldown and $CooldownTimer.time_left == 0:
-		onCooldown = false
-		$LocationMarkerBtn.disabled = false		
-		$AnimationPlayer.play("Active")
-
 func start_timer():
-	$LocationMarkerBtn.disabled = true
-	onCooldown = true
 	$AnimationPlayer.play("Cooldown")
-	$CooldownTimer.start()	
+	$LocationMarkerBtn.disabled = true
+	yield(get_tree().create_timer(10.0), "timeout")	
+	$LocationMarkerBtn.disabled = false
+	$AnimationPlayer.play("Active")	
