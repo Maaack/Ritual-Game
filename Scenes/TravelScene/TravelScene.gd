a9083b4791e18c36e8a5ea876fc742ac237ed32b
Maaack <marek.belski@gmail.com>
2022-10-22 17:56:27 -0700

    Made level progression work. Needed to add location data to each scene.

--- Scenes/TravelScene/TravelScene.gd
+++ Scenes/TravelScene/TravelScene.gd
@@ -5,19 +5,16 @@ func _load_ritual(location_data : LocationData) -> void:
 	SceneLoader.load_scene(location_data.ritual_scene_path)
 
 func _attach_signals():
-	for node in get_children():
+	var location_container = get_node("%LocationContainer")
+	for node in location_container.get_children():
 		if node.is_in_group("LocationMarkers"):
 			var locationbutton = node.get_node("LocationMarkerBtn")
 			locationbutton.connect("pressed", self, "_load_ritual", [node.location_data])
 
 func _ready():
 	_attach_signals()
-	
-	var player_level : int = 0
-	if not Config.has_section("Player"):
-		Config.set_config("Player", "Level", player_level)
-	
-	player_level = Config.get_config("Player", "Level", player_level)
+	GameLog.level_reached(0)
+	var player_level : int = GameLog.get_max_level_reached()
 	var location_container = get_node("%LocationContainer")
 	for child in location_container.get_children():
 		if child is LocationMarker:
